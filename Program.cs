5d2a61793b440e61532085aee72713192964ec06
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-04 14:04:03 +0000

    JCD1.4.2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace JCD1._4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Digite um número para a raiz quadrada: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            double raizQuadrada = Math.Sqrt(num);
+            Console.WriteLine("A raiz quadrada do número digitado é " + raizQuadrada);
+
+
+
+            //public double SquareRoot ()
+        }
+    }
+}
